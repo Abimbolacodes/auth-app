c255a3360b4ff49bb7e87a4f2ee229aa746b6f62
Abimbola Olatunji <161823517+Abimbolacodes@users.noreply.github.com>
2025-05-12 21:56:32 +0100

    Update App.jsx and other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthForm from './Components/AuthForm';
-import Home from './pages/Home';
+import Home from './Pages/Home';
 
 function App() {
   return (
